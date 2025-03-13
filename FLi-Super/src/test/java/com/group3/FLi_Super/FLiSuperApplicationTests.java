4b2e3f0e578a6049a90064e6ce0beacc1a179c18
IT23400986 <it23400986@my.sliit.lk>
2025-03-13 08:18:08 +0530

    Added Spring Boot boilerplate

---
+++ FLi-Super/src/test/java/com/group3/FLi_Super/FLiSuperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.FLi_Super;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FLiSuperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
