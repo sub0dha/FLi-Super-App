0b802760337b9613a65031900c0585f961ec53d1
nav2305 <it23226364@my.sliit.lk>
2025-03-18 11:29:58 +0530

    Revert "Added product controller"
    This reverts commit 9110fc1c834db20d10ee030d462d6418bc2a086c.

--- backend/src/main/java/org/group3/backend/controller/ProductController.java
+++ backend/src/main/java/org/group3/backend/controller/ProductController.java
@@ -5,8 +5,6 @@ import org.group3.backend.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 public class ProductController {
     
