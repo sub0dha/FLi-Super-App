9110fc1c834db20d10ee030d462d6418bc2a086c
nav2305 <it23226364@my.sliit.lk>
2025-03-18 11:20:50 +0530

    Added product controller

--- backend/src/main/java/org/group3/backend/controller/ProductController.java
+++ backend/src/main/java/org/group3/backend/controller/ProductController.java
@@ -5,6 +5,8 @@ import org.group3.backend.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class ProductController {
     
--- backend/src/main/java/org/group3/backend/model/Product.java
+++ backend/src/main/java/org/group3/backend/model/Product.java
@@ -1,11 +1,11 @@
 package org.group3.backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;    
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Getter
 @Setter
@@ -19,8 +19,56 @@ public class Product {
     private String description;
     private double price;
     private String category;
-    private double quantity;
+    private double stock_quantity;
 
     public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getStock_quantity() {
+        return stock_quantity;
+    }
+
+    public void setStock_quantity(double stock_quantity) {
+        this.stock_quantity = stock_quantity;
     }
 }
