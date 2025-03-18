7989aff54b3df7af8f8d7275a160f6996594fad2
nav2305 <it23226364@my.sliit.lk>
2025-03-18 11:38:36 +0530

    Revert "Product CRUD"
    This reverts commit 22eb21cf694fa9b4dc988275aba54d49c3f7e75b.

--- backend/src/main/java/org/group3/backend/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package org.group3.backend.repository;
-
-import org.group3.backend.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
