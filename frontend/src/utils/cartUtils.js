14c3e3dd971eee27c0a6d8af2b68ff0f8e0edb75
nav2305 <it23226364@my.sliit.lk>
2025-05-07 16:23:50 +0530

    Added some features to cartController

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -1,16 +1,17 @@
 export const getOrCreateCartId = async () => {
     let cartId = localStorage.getItem("cartId")
     if (!cartId) {
-      const res = await fetch("http://localhost:8080/cart", {
-        method: "POST",
-      })
-      if (!res.ok) {
-        throw new Error("Failed to create a new cart")
-      }
+        const res = await fetch("http://localhost:8080/cart", {
+            method: "POST",
+        })
 
-      const data = await res.json()
-      cartId = data.id
-      localStorage.setItem("cartId", cartId)
+        if (!res.ok) {
+            throw new Error("Failed to create a new cart")
+        }
+
+        const data = await res.json()
+        cartId = data.id
+        localStorage.setItem("cartId", cartId)
     }
     return cartId
 }
