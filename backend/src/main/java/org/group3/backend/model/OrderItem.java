ca438b7f09353b5ed26bf499b31e3a78bffa3ad3
Dilhara-Jayashan <it23279384@my.sliit.lk>
2025-05-07 15:10:42 +0530

    Update OrderItem.java

--- backend/src/main/java/org/group3/backend/model/OrderItem.java
+++ backend/src/main/java/org/group3/backend/model/OrderItem.java
@@ -4,3 +4,18 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Product product;
+
+    private int quantity;
+    private double subtotal;
+}
