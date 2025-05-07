062c20bb20f4f288d3a80a1e568053808d825888
Dilhara-Jayashan <it23279384@my.sliit.lk>
2025-05-07 15:01:32 +0530

    java

--- frontend/src/utils/CartUtils.js
+++ frontend/src/utils/CartUtils.js
@@ -1,17 +1,16 @@
 export const getOrCreateCartId = async () => {
-    let cartId = localStorage.getItem("cartId")
-    if (!cartId) {
-        const res = await fetch("http://localhost:8080/cart", {
-            method: "POST",
-        })
-
-        if (!res.ok) {
-            throw new Error("Failed to create a new cart")
-        }
-
-        const data = await res.json()
-        cartId = data.id
-        localStorage.setItem("cartId", cartId)
+  let cartId = localStorage.getItem("cartId")
+  if (!cartId) {
+    const res = await fetch("http://localhost:8080/cart", {
+      method: "POST",
+    })
+    if (!res.ok) {
+      throw new Error("Failed to create a new cart")
     }
-    return cartId
+
+    const data = await res.json()
+    cartId = data.id
+    localStorage.setItem("cartId", cartId)
+  }
+  return cartId
 }
